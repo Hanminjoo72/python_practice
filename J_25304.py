368384042039ed51313fb98fd80262395e586eeb
Hanminjoo72 <hmjhaha0207@naver.com>
2023-08-24 17:35:13 +0900

    second

--- J_25304.py
+++ J_25304.py
@@ -1,11 +1,11 @@
 H, M = map(int, input().split())
 
-if M < 45 :	# 분단위가 45분보다 작을 때 
-    if H == 0 :	# 0 시이면
+if M < 45 :	
+    if H == 0 :
         H = 23
-        M += 60
-    else :	# 0시가 아니면 (0시보다 크면)
+        M += 15
+    else :
         H -= 1	
-        M += 60
+        M += 15
         
-print(H, M-45)
+print(H, M)
