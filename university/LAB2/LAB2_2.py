e903b5d28b072505f302d2f7a95fd9537610f692
Hanminjoo72 <132250663+Hanminjoo72@users.noreply.github.com>
2024-01-06 17:33:22 +0900

    Delete university/LAB2/LAB2_2.py

--- university/LAB2/LAB2_2.py
+++
@@ -1,8 +0,0 @@
-radius = float(input('Enter the number: '))
-PI = float(input('Enter the PI: '))
-
-area = radius**2 * PI
-circumference = 2 * PI * radius
-
-print(f'area is {area:.2f}')
-print(f'circumference is {circumference:.2f}')
