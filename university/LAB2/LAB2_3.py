c34bb68239ec08a3a89fc618439c872d68305c8a
Hanminjoo72 <132250663+Hanminjoo72@users.noreply.github.com>
2024-01-06 17:33:28 +0900

    Delete university/LAB2/LAB2_3.py

--- university/LAB2/LAB2_3.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter the name: ")
-age = int(input(f"Enter the {name}'s age: "))
-
-generation = (f'{age // 10 * 10}대')
-
-if  age < 50:
-    print(f'{name}은 {generation}이다')
-else:
-    print(f'{name}은 50대 이상이다')
